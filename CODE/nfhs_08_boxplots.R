08ac643b1fbe122805a425ae98bb6b467739435f
kimbrone <kimbrone@seattleu.edu>
2022-01-12 15:50:56 -0800

    Added code for a different view of the boxplots (cropped the view)

--- CODE/nfhs_08_boxplots.R
+++ CODE/nfhs_08_boxplots.R
@@ -14,3 +14,16 @@ ggplot(data = urban_hhs, mapping = #What data?
 #would look like on their own, so I played around a bit and
 #came up with this:
 #Boxplot (Cropped/cleaned up)
+
+ggplot(data = urban_hhs, mapping =
+         aes(y = urban_type_labels,
+             x = hh_members)) +
+  geom_boxplot(outlier.shape = NA) + #To "zoom in"
+  coord_cartesian(xlim = c(0, 10)) + #To "crop" viewing area
+  xlab("Household Members")+
+  ylab("Type of Urban Area")
+
+#This makes me curious to learn and play further with what R
+#can do in terms of weighing this data, analyzing deeper
+#meaning behind the outliers... Alas, this is already so far
+#beyond my cability! One foot... two feet.
