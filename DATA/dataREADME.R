84044ad205317177d9b9a0cc1a3d330a50c49937
kimbrone <kimbrone@seattleu.edu>
2022-01-12 16:10:10 -0800

    no dta explanation

---
+++ DATA/dataREADME.R
@@ -0,0 +1,11 @@
+#The data file used is IAHR52FL.dta and, though it is in this
+#subdirectory, the gitignore file blocking dta files means
+#you can't see it! 
+
+#But it's there.
+
+#Lurking...
+
+#InVARIABLEy 
+
+#Sorry.
